ce69b16c8a1334e4da11c87bed137c96e3e233e4
Rodrigo Tavares <rodrigo.tavares.lima@hotmail.com>
2024-02-19 18:19:11 -0300

    test and logic problems

--- apps/back_rust/src/game.rs
+++ apps/back_rust/src/game.rs
@@ -9,13 +9,14 @@ pub struct Game<'a> {
     player_list: Vec<Player>,
     password: Option<String>,
     lands: Vec<Land<'a>>,
+    squared_size: u8,
     tps: u8,
     max_players: u8,
 }
 
 impl Game<'_> {
     pub fn new(squared_size: u8, tps: u8, password: Option<String>, max_players: u8) -> Self {
-        let range = 1..=squared_size;
+        let range = 1..=squared_size * squared_size;
 
         let lands = range
             .map(|_| Land {
@@ -27,6 +28,7 @@ impl Game<'_> {
         Self {
             lands,
             tps,
+            squared_size,
             password,
             max_players,
             player_list: vec![],
@@ -63,12 +65,69 @@ impl Game<'_> {
     }
 
     pub fn get_player_by_socket_id(&self, socket_id: String) -> Option<&Player> {
-        self.player_list
-            .iter()
-            .find(|p| p.socket_id == socket_id)
+        self.player_list.iter().find(|p| p.socket_id == socket_id)
     }
 
     fn get_land(&self, x: usize, y: usize) -> Option<&Land> {
-        todo!()
+        let lands_len = self.lands.len();
+        let maximum_axis_size = lands_len / 2;
+
+        if x > maximum_axis_size || y > maximum_axis_size {
+            return None;
+        }
+
+        let index = (y * self.squared_size as usize) + x;
+
+        return Some(&self.lands[index]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_game() {
+        let game = Game::new(10, 2, None, 2);
+        assert_eq!(game.lands.len(), 100);
+        assert_eq!(game.tps, 2);
+        assert_eq!(game.max_players, 2);
+        assert_eq!(game.password, None);
+    }
+
+    #[test]
+    fn get_the_first_land() {
+        let game = Game::new(10, 2, None, 2);
+        let target = &game.lands[0];
+        let result = game.get_land(0, 0).unwrap();
+
+        assert_eq!(result as *const Land, target as *const Land);
+    }
+
+    #[test]
+    fn get_the_seconnd_land() {
+        let game = Game::new(10, 2, None, 2);
+        let target = &game.lands[1];
+        let result = game.get_land(1, 0).unwrap();
+
+        assert_eq!(result as *const Land, target as *const Land);
+    }
+
+    #[test]
+    fn get_the_first_from_second_row() {
+        let game = Game::new(10, 2, None, 2);
+        let target = &game.lands[10];
+        let result = game.get_land(0, 1).unwrap();
+
+        assert_eq!(result as *const Land, target as *const Land);
+    }
+
+    #[test]
+    fn get_the_last_land() {
+        let game = Game::new(10, 2, None, 2);
+        let target = &game.lands[99];
+        let result = game.get_land(9, 9).unwrap();
+
+        assert_eq!(result as *const Land, target as *const Land);
     }
 }
